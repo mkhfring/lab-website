a3c734818275e8c8002f4aef639c7cbb1e9d1504
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-07-16 02:19:03 -0700

    The team link is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { MemberComponent } from './components/member/member.component';
+import { MembersComponent } from './components/members/members.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: MainComponent},
+  {path: 'members', component: MembersComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
