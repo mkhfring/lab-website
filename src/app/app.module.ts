878e73bdb0d831b3cc73ce8d063347c8803ed2a3
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-07-17 02:13:05 -0700

    Trying to solve the server problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { ResearchComponent } from './components/research/research.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ResearchComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
