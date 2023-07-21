711d1d6baefabc0778660686dc7fe55a425603cc
Mohamad Khajezade <khajezade.mohamad@gmail.com>
2023-07-20 21:30:58 -0700

    Fixing the error for fontawsome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { MainComponent } from './components/main/main.component';
 import { ResearchComponent } from './components/research/research.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -21,8 +19,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
